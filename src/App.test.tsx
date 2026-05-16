357e29ebac20959e873c556dbe8e786c60ba34fd
Tatsiana Hladkaya <tatyana.hladkaya@gmail.com>
2026-05-16 21:04:26 +0300

    fix: fix App.tes.t.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,9 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
+import { createMemoryRouter, RouterProvider } from 'react-router';
 import { vi } from 'vitest';
 import App from './App';
+import MainPage from './pages/mainPage';
 
 vi.mock('./pages/mainPage', () => ({
   default: () => <div data-testid="main-page">Main Page</div>,
@@ -9,7 +11,20 @@ vi.mock('./pages/mainPage', () => ({
 
 describe('App', () => {
   it('renders MainPage', () => {
-    render(<App />);
+    const router = createMemoryRouter([
+      {
+        path: '/',
+        element: <App />,
+        children: [
+          {
+            index: true,
+            element: <MainPage />,
+          },
+        ],
+      },
+    ]);
+
+    render(<RouterProvider router={router} />);
 
     expect(screen.getByTestId('main-page')).toBeInTheDocument();
   });
