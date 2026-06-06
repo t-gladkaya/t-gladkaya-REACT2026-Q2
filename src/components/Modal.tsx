a9f888985983687b1b47bd6326caf8361b1ceb08
Tatsiana Hladkaya <tatyana.hladkaya@gmail.com>
2026-06-06 18:05:54 +0300

    style: add styles to modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -10,14 +10,14 @@ export default function Modal({ isOpen, onClose, children }: ModalProps) {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 p-4">
-      <div className="bg-white p-40 rounded-lg shadow-lg">
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 p-4">
+      <div className="relative bg-white p-40 rounded-lg shadow-lg">
         <button
           type="button"
-          className="absolute top-2 right-2 text-white hover:bg-slate-700"
+          className="absolute top-2 right-2 text-white"
           onClick={onClose}
         >
-          ← Back
+          <img src="/close.svg" alt="Close" className="w-6 h-6 hover:scale-110 hover:cursor-pointer transition duration-300 ease-in-out " />
         </button>
         {children}
       </div>
